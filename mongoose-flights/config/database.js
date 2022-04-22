4279fc3cf42890a981cbb9e10f01e98c227b85af
akhthomas <ms.kellanthomas@gmail.com>
2022-04-22 16:31:16 -0700

    add schema

--- mongoose-flights/config/database.js
+++ mongoose-flights/config/database.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose'); 
 
+// shortcut to the mongoose.Schema class
+// optional but convenient !
+const Schema = mongoose.Schema;
+
 mongoose.connect('mongodb://localhost/flights', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+// a shortcut to our mongoose.connection object
+const db = mongoose.connection; 
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
