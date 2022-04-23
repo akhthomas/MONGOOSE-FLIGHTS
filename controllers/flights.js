b6734aa340330d99579a23bd12255c97d3ab1b90
akhthomas <ms.kellanthomas@gmail.com>
2022-04-22 19:42:22 -0700

    route and view add

---
+++ controllers/flights.js
@@ -0,0 +1,8 @@
+module.exports = {
+    new: newFlight
+};
+
+
+function newFlight(req, res) {
+    res.render('flights/new');
+}
---
+++ routes/flights.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const flightCtrl = require('../controllers/flights');
+
+router.get('/new', flightCtrl.new);
+
+
+
+module.exports = router;
