a56c77559db6c8e6b196eecc5a87752a91e1bf5a
akhthomas <ms.kellanthomas@gmail.com>
2022-04-22 18:30:39 -0700

    add model, things are functioning

--- mongoose-flights/config/database.js
+++ mongoose-flights/config/database.js
@@ -4,6 +4,37 @@ const mongoose = require('mongoose');
 // optional but convenient !
 const Schema = mongoose.Schema;
 
+// https://mongoosejs.com/docs/validation.html
+const flightSchema = new Schema({
+     airline: { 
+       type: String, 
+       enum: ['American', 'Southwest', 'United'],
+       required: true 
+      },
+
+     airport: { 
+       type: String, 
+       enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+       default: 'DEN', 
+       required: true 
+      },
+
+      flightNo: { 
+        type: Number,
+        min: 10,
+        max: 9999,
+        required: true,
+      },
+        
+      departs: {
+        type: Date,
+        default: function() {
+          return new Date().getNextYear();
+        }
+      }
+});
+
+
 mongoose.connect('mongodb://localhost/flights', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
